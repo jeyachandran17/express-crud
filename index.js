5bf2f900aff5cc9678e89cbaae27c2e50cea7f08
jeyachandran17 <jeyachandran1733@gmail.com>
2023-03-04 02:09:23 +0530

    vercel setup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 // const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL;
